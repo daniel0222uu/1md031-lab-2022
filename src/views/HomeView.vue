a5b6a6e63e753ccc548ee0227aae255baff811f5
Daniel <danielceoca@AirsomtrDaniel2.lan>
2022-11-08 22:53:08 +0100

    Nu är allt som det ska vara för tasken när man ska kopiera över CSS+HTML filerna

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,202 @@
 <template>
-  <div>
-    <div>
-      Burgers
-      <Burger v-for="burger in burgers"
-              v-bind:burger="burger" 
-              v-bind:key="burger.name"/>
+  <!DOCTYPE HTML>
+  <html lang="en">
+  <header class>
+
+    <title>This is the page title</title>
+    <meta charset="utf-8"/>
+  </header>
+
+  <div id="wholediv">
+
+
+
+
+    <div id="longImagediv" >
+      <img src="https://as1.ftcdn.net/v2/jpg/03/02/41/48/1000_F_302414819_juA2QCzXIICStqJOUHQicECan4WKZvN9.jpg" id="longImage">
+      <div class="centerLongImageText">
+        <h1 class="ourHeader">Welcome to Nonexistant Burgers</h1>
+      </div>
+
+
+
     </div>
-    <div id="map" v-on:click="addOrder">
-      click here
+
+
+
+
+    <div class="welcomeArea separateArea welcomingText"> <link rel="stylesheet" type="text/css" href="style.css">
+
+
+
+
+      <div>
+        <header>
+
+          <p>
+            <strong> Select burger</strong>
+          </p>
+          <p>This is where you execute your burger selection
+          </p>
+        </header>
+      </div>
+
+
+
+
+      <div class="burgerMenu welcomeArea welcomingText wrapper"> <!--our entire burger menu -->
+        <div class="box fire"> <!-- fire burger div -->
+          <p>
+            <strong>The fire burger </strong> </p>
+          <p>
+            <img src="https://i.ytimg.com/vi/i7qI52gVPlA/maxresdefault.jpg" style="width:180px;height:180px;">
+          </p>
+          <p>
+            Ingredients:
+          </p>
+          <ul class="fatText">
+            <li> Avocado</li>
+            <li> Tomato</li>
+          </ul>
+        </div>
+
+        <div class="box dank"> <!-- dank burger div -->
+          <p>
+            <strong>The dank burger </strong>
+          </p>
+          <p> <img src="https://californiaavocado.com/wp-content/uploads/2021/03/Classic-Calif-Avocado-Cheeseburger-with-Carmelized-Onions-CAC-001-cropped-scaled.jpg" style="width:180px;height:180px;"></p>
+          <p>
+            Ingredients:
+          </p>
+          <ul class="fatText">
+            <li> Cheese, dairy</li>
+            <li> Onion</li>
+            <li> Mayonaise, dairy</li>
+            <li> Bacon, pork</li>
+          </ul>
+        </div>
+
+        <div class="box chickenburger" > <!-- chicken burger div -->
+          <p>
+            <strong>The chicken burger </strong>
+          </p>
+          <p> <img src="https://thescranline.com/wp-content/uploads/2022/02/crispy-chicken-burger-WEB-01.jpg" style="width:180px;height:180px;"></p>
+
+          <p>
+            Ingredients:
+          </p>
+          <ul class="fatText">
+            <li> Cheese</li>
+            <li> Lettuce</li>
+          </ul>
+        </div>
+
+      </div>
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+    <main>
+
+      <div class="separateArea" id="customerDiv"> <!-- customer div -->
+
+        <p>
+          <strong>
+            Customer information
+          </strong>
+        </p>
+        <p>
+          This is where you provide necessary information
+        </p>
+
+
+        <section id="contact">
+          <form>
+            <p>
+              <label for="firstname">Full name</label><br>
+              <input type="text" id="firstname" name="fn" required="required" placeholder="First and last name">
+            </p>
+            <p>
+              <label for="email_adress">Email adress</label><br>
+              <input type="text" id="email_adress" name="em" required="required" placeholder="Email adress">
+            </p>
+            <p>
+              <label for="street_name">Street</label><br>
+              <input type="text" id="street_name" name="sname" required="required" placeholder="Street name">
+            </p>
+            <p>
+              <label for="street_number">House</label><br>
+              <input type="number" id="street_number" name="snumber" required="required" placeholder="House number">
+            </p>
+          </form>
+
+          <label for="recipient">Payment method</label>
+          <select id="recipient" name="Klarna">
+            <option>Swish</option>
+            <option>VISA / Mastercard</option>
+            <option>Klarna</option>
+          </select>
+
+          <fieldset>
+            <legend>Specify your gender</legend>
+
+            <div class="genderButtons">
+              <input type="radio" id="Male" name="drone" value="Male"
+              >
+              <label for="Male">Male</label>
+            </div>
+
+            <div class="genderButtons">
+              <input type="radio" id="Female" name="drone" value="Female">
+              <label for="Female">Female</label>
+            </div>
+
+            <div class="genderButtons">
+              <input type="radio" id="nonbinary" name="drone" value="nonbinary" >
+              <label for="nonbinary">Other</label>
+            </div>
+
+          </fieldset>
+
+          <button type="submit">
+            <img src="https://www.pngall.com/wp-content/uploads/12/Order-Now-Button-PNG-Photo.png" style="width:300px;height:100px;" >
+          </button>
+
+
+        </section>
+
+      </div>
+    </main>
+
+
+    <hr>
+    <footer>
+      <p> ® Nonexistant burgers AB</p>
+    </footer>
+    <div>
+      <div>
+        Burgers
+        <Burger v-for="burger in burgers"
+                v-bind:burger="burger"
+                v-bind:key="burger.name"/>
+      </div>
+      <div id="map" v-on:click="addOrder">
+        click here
+      </div>
     </div>
   </div>
+
+
+  </html>
+
 </template>
 
 <script>
@@ -29,13 +216,10 @@ class MenuItem{
   }
 }
 
-let burger = new MenuItem("fire","https://bildix.mmcloud.se/bildix/api/images/4c7ccd16-4dda-40db-b683-7a6bc14d82ff.jpeg?fit=crop&w=980&h=551",400,true)
 
 const burgerArray = [new MenuItem("fire","https://bildix.mmcloud.se/bildix/api/images/4c7ccd16-4dda-40db-b683-7a6bc14d82ff.jpeg?fit=crop&w=980&h=551", 400,true),
-  new MenuItem("dank","https://bildix.mmcloud.se/bildix/api/images/4c7ccd16-4dda-40db-b683-7a6bc14d82ff.jpeg?fit=crop&w=980&h=551", 700,false),
-  new MenuItem("chicken","https://bildix.mmcloud.se/bildix/api/images/4c7ccd16-4dda-40db-b683-7a6bc14d82ff.jpeg?fit=crop&w=980&h=551", 250,false)];
-
-
+  new MenuItem("dank","https://bildix.mmcloud.se/bildix/api/images/4c7ccd16-4dda-40db-b683-7a6bc14d82ff.jpeg?fit=crop&w=980&h=551", 400,true),
+  new MenuItem("chicken","https://bildix.mmcloud.se/bildix/api/images/4c7ccd16-4dda-40db-b683-7a6bc14d82ff.jpeg?fit=crop&w=980&h=551", 400,true)]
 
 
 
@@ -71,6 +255,112 @@ export default {
 </script>
 
 <style>
+
+@media screen and (max-width: 800px) {
+  h1 {
+    font-size: 6vw;
+  }
+}
+h1{
+  font-size: 6vw;
+}
+
+<!--här var det importer förut men inte längre-->
+
+
+body {
+  font-family: 'Island Moments', cursive;
+  font-family: 'Mukta', sans-serif;
+}
+
+.welcomingText{
+  font-family: Helvetica;
+  color: white;
+}
+
+.welcomeArea{
+  background-color:black;
+}
+
+button:hover{
+  background-color:springgreen;
+  outline-style: auto;
+  box-shadow: 0 0 0 2px #5a01a7;
+  transition: 0.2s;
+}
+
+.genderButtons{
+  margin-top: 20px;
+}
+
+.burgerMenu{
+  margin-left: 300px;
+}
+
+.separateArea{
+  border: 2px solid orange;
+  margin: 10px;
+  padding-left: 40px;
+
+}
+
+
+#customerDiv{
+  background-color: white;
+  color: black    ;
+}
+
+#wholediv{
+}
+
+
+#longImage{
+  width: 100%;
+  opacity: 40%;
+  height: 300px;
+}
+
+#longImagediv{
+  position: relative;
+  text-align: center;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+
+.wrapper{
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 200px 200px 200px;
+  grid-gap: 80px;
+}
+
+.box{
+  padding: 20px;
+}
+
+.fire{
+  grid-column: 1;
+}
+
+.dank{
+  grid-column: 2;
+}
+
+.chickenburger{
+  grid-column: 3;
+}
+
+.centerLongImageText{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+}
+
+.fatText{
+  font-weight: bold;
+}
   #map {
     width: 300px;
     height: 300px;
