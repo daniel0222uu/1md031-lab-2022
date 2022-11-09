5574d773c29aa3407a72dc5d2ba14a812055cfd3
Daniel <danielceoca@AirsomtrDaniel2.lan>
2022-11-09 15:33:42 +0100

    Nu är oneburger med mikaels kod igen

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,17 +1,6 @@
 <template>
-  <div class="box fire"><!-- fire burger div -->
-    <p id="burgerName">
-      <strong> The fire burger </strong> </p>
-    <p>
-      <img src="https://i.ytimg.com/vi/i7qI52gVPlA/maxresdefault.jpg" style="width:180px;height:180px;">
-    </p>
-    <p>
-      Ingredients:
-    </p>
-    <ul class="fatText">
-      <li> Avocado</li>
-      <li> Tomato</li>
-    </ul>
+  <div>
+    {{ burger.name }} {{ burger.kCal }}
   </div>
   </template>
   
