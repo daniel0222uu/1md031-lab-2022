24cb9852a8da23c0bf6c1ec29d04f56da83e538f
Daniel <danielceoca@AirsomtrDaniel2.lan>
2022-11-09 15:07:14 +0100

    nu är oneburger ändrat med

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,7 +1,18 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
+  <div class="box fire"><!-- fire burger div -->
+    <p id="burgerName">
+      <strong> The fire burger </strong> </p>
+    <p>
+      <img src="https://i.ytimg.com/vi/i7qI52gVPlA/maxresdefault.jpg" style="width:180px;height:180px;">
+    </p>
+    <p>
+      Ingredients:
+    </p>
+    <ul class="fatText">
+      <li> Avocado</li>
+      <li> Tomato</li>
+    </ul>
+  </div>
   </template>
   
   <script>
